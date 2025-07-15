521af25d5f6e90fa4463320a5fec54aea7ba3ab0
kosm6966 <kosm6966@colorado.edu>
2025-07-15 11:14:50 -0600

    changed incore behavior and cmake compat.

--- pyscf/pbc/scf/hf.py
+++ pyscf/pbc/scf/hf.py
@@ -646,7 +646,7 @@ class SCF(mol_hf.SCF):
 
         if (not omega and kpts_band is None and
             # TODO: generate AO integrals with rsjk algorithm
-            not self.rsjk and
+            not self.rsjk and not self.with_df and
             (self.exxdiv == 'ewald' or not self.exxdiv) and
             (self._eri is not None or cell.incore_anyway or
              self._is_mem_enough())):
